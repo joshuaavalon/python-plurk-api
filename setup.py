7f186d5cebaa382398c85fddcc349a8e702fc483
Joshua <joshuaavalon@gmail.com>
2017-09-08 16:16:21 +0800

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    name="plurk-api",
+    packages=["plurk-api"],
+    version="1.0.0",
+    description="Plurk API",
+    author="Joshua Avalon",
+    author_email="joshuaavalon@gmail.com",
+    url="https://github.com/joshuaavalon/python-plurk-api",
+    download_url="https://github.com/joshuaavalon/python-plurk-api/archive/1.0.0.tar.gz",
+    keywords=["plurk", "oauth"],
+    classifiers=[],
+    requires=["oauth2"]
+)
