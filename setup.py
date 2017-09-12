45ab6483843c017e9521bdb638ce691b30fab6d9
Joshua <joshuaavalon@gmail.com>
2017-09-12 23:23:57 +0800

    Create requirements.txt

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup
 
 setup(
-    name="plurk-api",
+    name="plurk_api",
     packages=["plurk_api"],
-    version="1.0.1",
+    version="2.0.0",
     description="Plurk API",
     author="Joshua Avalon",
     author_email="joshuaavalon@gmail.com",
@@ -12,5 +12,5 @@ setup(
     classifiers=[],
     license="MIT",
     python_requires=">=3",
-    requires=["oauth2"]
+    requires=["oauth2", "typing"]
 )
